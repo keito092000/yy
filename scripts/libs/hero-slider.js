70632d0e139d11b3af0faf6a670c74f018b5ab89
keito092000 <77962525+keito092000@users.noreply.github.com>
2021-01-25 19:00:46 +0900

    Add files via upload

---
+++ scripts/libs/hero-slider.js
@@ -0,0 +1,32 @@
+class HeroSlider {
+    constructor(el) {
+        this.el = el;
+        this.swiper = this._initSwiper();
+    }
+
+    _initSwiper() {
+        return new Swiper(this.el, {
+            // Optional parameters
+            // direction: 'vertical',
+            grabCursor: true,
+            loop: true,
+            effect: 'fade',
+            centeredSlides: true,
+            slidesPerView: 1,
+            speed: 1000,
+        });
+    }
+
+    start(options = {}) {
+        options = Object.assign({
+            delay: 4000,
+            disableOnInteraction: false
+        }, options);
+        
+        this.swiper.params.autoplay = options;
+        this.swiper.autoplay.start();
+    }
+    stop() {
+        this.swiper.autoplay.stop();
+    }
+}
